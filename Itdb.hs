d7b9115b3fa87e855c96a9b76afd7c65a0b932db
Etienne Millon <etienne.millon@gmail.com>
2012-04-10 20:50:59 +0200

    FFI stub for itdb_parse

--- Itdb.hs
+++ Itdb.hs
@@ -8,7 +8,22 @@ module Itdb ( Itdb
             )
     where
 
-data Itdb
+import Foreign.C.String
+import Foreign.Marshal.Alloc
+import Foreign.Ptr
+
+data ItdbStruct
+
+data GError
+
+gErrorFail :: Ptr (Ptr GError) -> IO (Either String a)
+gErrorFail ge = fail "gErrorMessage"
+
+foreign import ccall "itdb.h itdb_parse" c_itdb_parse :: CString
+                                                      -> Ptr (Ptr GError)
+                                                      -> IO (Ptr ItdbStruct)
+
+type Itdb = Ptr ItdbStruct
 
 data Track = Track { trackArtist :: Maybe String
                    , trackAlbum :: Maybe String
@@ -19,8 +34,14 @@ data Playlist = Playlist { playlistName :: String
                          , playlistMembers :: [Track]
                          }
 
-itdbParse :: FilePath -> IO (Either String a)
-itdbParse _ = return $ Left "itdbParse : not implemented"
+itdbParse :: FilePath -> IO (Either String Itdb)
+itdbParse fp =
+  alloca $ \ perr ->
+  withCString fp $ \ cs -> do
+    p <- c_itdb_parse cs perr
+    if p == nullPtr
+      then gErrorFail perr
+      else return $ Right p
 
 itdbPlaylists :: Itdb -> IO [Playlist]
 itdbPlaylists db = fail "itdbPlaylists : not implemented"
